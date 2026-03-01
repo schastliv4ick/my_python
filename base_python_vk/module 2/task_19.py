48af08f4d6a9ecb48d073345c1b201a10c548ef7
Maria Koltakova <kol06m@yandex.ru>
2026-03-01 21:30:09 +0300

    decorator solution

--- base_python_vk/module 2/task_19.py
+++ base_python_vk/module 2/task_19.py
@@ -1,5 +1,12 @@
 def repeat_deco(n=1):
-   # YOUR CODE HERE
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for I in range(n):
+                result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
+   
 
 code = []
 while data := input():
